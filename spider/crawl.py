487c1665c0d5541c6cf6fc215c2a00d3dc842db4
Gavin <tianyu_0723@hotmail.com>
2017-07-31 18:14:44 +0800

    爬虫基类

---
+++ spider/crawl.py
@@ -0,0 +1,33 @@
+# -*- coding: UTF-8 -*-
+import requests
+from config import get_http_header
+
+
+class Crawl(object):
+    def __init__(self):
+        self._session = requests.session()
+        self._session.headers.update(get_http_header())
+        self._request_kwargs = {
+            'timeout': 5
+        }
+
+    def _transparent(self):
+        raise NotImplementedError
+
+    def _anonymous(self):
+        raise NotImplementedError
+
+    def _http(self):
+        raise NotImplementedError
+
+    def _https(self):
+        raise NotImplementedError
+
+    def _text(self, url):
+        raise NotImplementedError
+
+    def _parse(self, text):
+        raise NotImplementedError
+
+    def generator(self):
+        raise NotImplementedError
