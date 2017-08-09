bb5e21fc755fbb3270a26f0612c3d6ee118ee471
Gavin <tianyu_0723@hotmail.com>
2017-08-09 18:03:26 +0800

    redis 实现修改

--- persistence/redis_impl.py
+++ persistence/redis_impl.py
@@ -6,7 +6,11 @@ from config import PERSISTENCE
 
 class Redis(Base):
     def __init__(self):
+        # redis 操作客户端
         self._client = pyRedis.from_url(PERSISTENCE['url'])
+
+        # 可搜索项
+        self._index_keys = ('anonymity', 'protocol', 'speed')
 
     def list(self):
         pass
@@ -14,15 +18,23 @@ class Redis(Base):
     def get(self):
         pass
 
+    def add(self, data):
+        # 存储代理
+        proxy_key = 'proxy_%s:%s_%s' % (data['ip'], data['port'], data['protocol'])
+        self._client.hmset(proxy_key, data)
+
+        # 存储索引,以便搜索
+        for key in self._index_keys:
+            if key == 'speed':
+                self._client.zadd('index_speed', proxy_key, data['speed'])
+            else:
+                self._client.sadd('index_%s_%s' % (key, data[key]), proxy_key)
+
     def update(self):
         pass
 
     def delete(self):
         pass
-
-    def add(self):
-
-        self._client.set('far', 'bor')
 
     def handler(self):
         return self._client
@@ -31,3 +43,4 @@ class Redis(Base):
 if __name__ == '__main__':
     redis = Redis()
     handler = redis.handler()
+    print(handler.set('a', 'b'))
