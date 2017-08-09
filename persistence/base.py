3f4495167f1e85d54023a2479626e6d86b39c385
Gavin <tianyu_0723@hotmail.com>
2017-08-09 18:03:15 +0800

    持久化基类修改

--- persistence/base.py
+++ persistence/base.py
@@ -1,13 +1,19 @@
 # -*- coding: UTF-8 -*-
 class Base(object):
+    def list(self):
+        raise NotImplementedError
+
     def get(self):
         raise NotImplementedError
 
-    def add(self):
+    def add(self, data):
+        raise NotImplementedError
+
+    def update(self):
         raise NotImplementedError
 
     def delete(self):
         raise NotImplementedError
 
-    def update(self):
+    def handler(self):
         raise NotImplementedError
