aa5d4b651781af5f444131a36909c5955e5a576f
Gavin <tianyu_0723@hotmail.com>
2017-08-09 18:04:23 +0800

    爬取西刺代理实现

--- spider/crawl_xici.py
+++ spider/crawl_xici.py
@@ -1,6 +1,9 @@
 # -*- coding: UTF-8 -*-
 from spider.crawl import Crawl
 from lxml import etree
+import time
+import json
+import requests
 
 
 class XiCi(Crawl):
@@ -8,7 +11,27 @@ class XiCi(Crawl):
         Crawl.__init__(self)
 
     def _transparent(self):
-        pass
+        url = 'http://www.xicidaili.com/nt/%u'
+        done = False
+        page = 1
+        fail = 0
+        while not done:
+            text = self._text(url % page)
+            if text is not None:
+                proxies = self._parse(text)
+                if proxies is not None:
+                    for proxy in proxies:
+                        proxy = self._connectable(proxy)
+                        if proxy:
+                            yield proxy
+                    page += 1
+                else:
+                    fail += 1
+            else:
+                fail += 1
+            # 如果失败大于5次,停止爬取
+            if fail > 5 or page == 5:
+                done = True
 
     def _anonymous(self):
         url = 'http://www.xicidaili.com/nn/%u'
@@ -21,27 +44,22 @@ class XiCi(Crawl):
                 proxies = self._parse(text)
                 if proxies is not None:
                     for proxy in proxies:
-                        proxy['anonymity'] = 'anonymous'
-                        print(proxy)
+                        proxy = self._connectable(proxy)
+                        if proxy:
+                            yield proxy
                     page += 1
                 else:
                     fail += 1
             else:
                 fail += 1
             # 如果失败大于5次,停止爬取
-            if fail > 5:
+            if fail > 5 or page == 5:
                 done = True
 
-    def _http(self):
-        pass
-
-    def _https(self):
-        pass
-
     def _text(self, url):
-        r = self._session.get(url, **self._request_kwargs)
-        if r.ok:
-            return r.text
+        response = self._session.get(url, **self._request_kwargs)
+        if response.ok:
+            return response.text
         else:
             # TODO 使用代理重新尝试下载
             return None
@@ -79,22 +97,85 @@ class XiCi(Crawl):
                 'country': country,
                 'address': address,
                 'anonymity': '',  # anonymous transparent
-                'protocol': protocol,  # http https http/https
-                'speed': 0.2
+                'protocol': protocol,  # http https http&https
+                'speed': 0.00
             }
 
             proxies.append(proxy)
 
         return proxies
 
-    def generator(self, anonymity='http'):
-        pass
+    def _connectable(self, proxy):
+        """
+        验证可连接性,返回此代理的类型等数据
+        :param proxy:
+        :return:
+        """
+
+        def check(u, p):
+            try:
+                start_point = time.time()
+                response = requests.get(u, proxies={
+                    'http': 'http://%s:%s' % (p['ip'], p['port']),
+                    'https': 'http://%s:%s' % (p['ip'], p['port'])
+                }, **self._request_kwargs)
+                if response.ok:
+                    interval = round(time.time() - start_point, 2)
+                    res_json = json.loads(response.text)
+                    ip = res_json['origin']
+                    if ',' in ip:
+                        anonymity = 'transparent'
+                    else:
+                        anonymity = 'anonymous'
+                    return True, anonymity, interval
+                else:
+                    return False, False, False
+            except Exception:
+                return False, False, False
+
+        http, h_anonymity, h_interval = check('http://httpbin.org/get', proxy)
+        https, hs_anonymity, hs_interval = check('https://httpbin.org/get', proxy)
+
+        if http and https:
+            proxy['protocol'] = 'http&https'
+            proxy['anonymity'] = h_anonymity
+            proxy['speed'] = h_interval
+        elif http:
+            proxy['protocol'] = 'http'
+            proxy['anonymity'] = h_anonymity
+            proxy['speed'] = h_interval
+        elif https:
+            proxy['protocol'] = 'https'
+            proxy['anonymity'] = hs_anonymity
+            proxy['speed'] = hs_interval
+        else:
+            proxy = False
+
+        return proxy
+
+    def generator(self, anonymity='anonymous'):
+        if anonymity is 'anonymous':
+            return self._anonymous()
+        elif anonymity is 'transparent':
+            return self._transparent()
+        else:
+            return dict()
 
     def test(self):
         # self._parse(self._content('http://www.xicidaili.com/nn/1'))
-        self._anonymous()
+        # for item in self.generator(anonymity='anonymous'):
+        #     print(item)
+        return self._connectable(proxy={
+            'ip': '47.94.81.119',
+            'port': '8888'
+        })
 
 
 if __name__ == '__main__':
     c = XiCi()
-    c.test()
+    # c.test()
+    res = c.test()
+    if res.ok:
+        print(res.text)
+    else:
+        print('fail')
